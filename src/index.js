a726172eca53773bba7629feb074c8b9ad73f1ff
Oleg V. Anisov <anisov@obninsk.ru>
2020-02-04 22:12:04 +0300

    change isArray to instanceof

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@
    isAllTrue([100, 2, 3, 4, 5], n => n < 10) // вернет false
  */
 function isAllTrue(array, fn) {
-    if (!Array.isArray(array) || array.length === 0) {
+    if (!(array instanceof Array) || array.length === 0) {
         throw 'empty array';
     } else if (typeof fn !== 'function') {
         throw 'fn is not a function';
@@ -51,7 +51,7 @@ function isAllTrue(array, fn) {
    isSomeTrue([1, 2, 3, 4, 5], n => n > 20) // вернет false
  */
 function isSomeTrue(array, fn) {
-    if (!Array.isArray(array) || array.length === 0) {
+    if (!(array instanceof Array) || array.length === 0) {
         throw 'empty array';
     } else if (typeof fn !== 'function') {
         throw 'fn is not a function';
