81b3cba37d8680e62766b9eb808ca87faba5e85e
Oleg V. Anisov <anisov@obninsk.ru>
2020-02-12 17:10:43 +0300

    change once

--- src/index.js
+++ src/index.js
@@ -77,7 +77,12 @@ function delegate(target, fn) {
    once(document.querySelector('button'), () => console.log('обработчик выполнился!')) // добавит такой обработчик кликов для указанного элемента, который вызовется только один раз и затем удалится
  */
 function once(target, fn) {
-    target.addEventListener('click', fn, { once: true });
+    let handler = e => {
+        target.removeEventListener('click', handler);
+        fn(e);
+    };
+
+    target.addEventListener('click', handler);
 }
 
 export {
